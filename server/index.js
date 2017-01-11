26b83773976a470d9e586cad7f639a041db8a28d
juanjomiron <juanjo5577@gmail.com>
2017-01-11 12:37:24 +0100

    añadido index.js y conexion con socket

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var express = require ('express');
+var app = express();
+var server = require ('http').Server(app);
+var io =require('socket.io')(server);
+
+app.get('/hola-mundo', function(req, res){
+    res.status(200).send('Hola mundo desde una ruta');
+});
+
+server.listen(6677, function(){
+              console.log('Servidor esta funcionando en http://localhost:6677');
+});
+
